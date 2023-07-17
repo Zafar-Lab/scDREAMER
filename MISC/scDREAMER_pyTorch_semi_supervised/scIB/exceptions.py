6d7d41a56a44f4e2c31d9acecd614e1ec48f0ad2
ajitashree <ajita92@gmail.com>
2023-07-17 20:35:27 +0530

    clean

--- MISC/scDREAMER_pyTorch_semi_supervised/scIB/exceptions.py
+++
@@ -1,7 +0,0 @@
-class IntegrationMethodNotFound(ModuleNotFoundError):
-
-    def __init__(self, exception):
-        self.message = f"\n{exception.name} is not installed by default, " \
-                       "please make sure you install it manually."
-        super().__init__(self.message)
-

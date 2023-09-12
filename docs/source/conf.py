8762a33ddfaac6be74e2e48a7a714da824f85c7c
krushnapavan9 <krushnapavan9@gmail.com>
2023-09-13 00:02:27 +0530

    readthedocs tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.duration', 'sphinx.ext.autodoc'
+   'nbsphinx', 'sphinx.ext.duration', 'sphinx.ext.autodoc'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
